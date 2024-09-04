ce6cc5fa0117996a4606d509c8b0ef6f75f77c19
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2024-09-04 11:43:22 +0200

    fix warning + comment + default event type

--- Assets/Plugins/StreamChat/Core/StatefulModels/IStreamMessage.cs
+++ Assets/Plugins/StreamChat/Core/StatefulModels/IStreamMessage.cs
@@ -33,7 +33,7 @@ namespace StreamChat.Core.StatefulModels
         IReadOnlyList<StreamMessageAttachment> Attachments { get; }
 
         /// <summary>
-        /// Channel unique identifier in &lt;type&gt;:&lt;id&gt; format
+        /// Channel unique identifier in type:id format
         /// </summary>
         string Cid { get; }
 
